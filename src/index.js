c69a2ee6514ac014f82255b241d028e67318da33
elena-lucky <elena.ivanovada@gmail.com>
2023-08-22 00:06:30 +0300

    feat: add function toeadable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function toReadable (number) {
-  
+  let words = [ "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+  "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", ];
+  let tyWords = [ "", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety", ];
+  if (number === 0) {
+    return "zero";
+  } else if (number < 20) {
+    return words[number];
+  } else if (number < 100) {
+    return tyWords[Math.floor(number / 10)] + (number % 10 !== 0 ? " " + words[number % 10] : "");
+  } else {
+    return words[Math.floor(number / 100)] + " hundred" + (number % 100 !== 0 ? " " + toReadable(number % 100) : "");
+}
 }
